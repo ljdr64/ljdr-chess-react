71ef3c7c6d34d87b54edd6c5d120148e52518b16
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-06-17 10:14:21 -0300

    feat: validation for pawn movements

--- src/ChessMoves/pawnMoves.js
+++ src/ChessMoves/pawnMoves.js
@@ -7,8 +7,31 @@ export function getPawnMoves(fromPiece, square, fromPosition, board) {
   const fromFile = fromPosition[0].charCodeAt(0) - 'a'.charCodeAt(0);
   const fromRank = 8 - fromPosition[1];
 
-  if (file === fromFile && Math.abs(rank - fromRank) === 1) {
-    legalMoves = true;
+  if (fromPiece === 'P') {
+    if (board[rank][file] === 'empty') {
+      if (
+        file === fromFile &&
+        (fromRank - rank === 1 || (fromRank === 6 && fromRank - rank === 2))
+      ) {
+        legalMoves = true;
+      }
+    } else if (Math.abs(file - fromFile) === 1 && fromRank - rank === 1) {
+      legalMoves = true;
+    }
   }
+
+  if (fromPiece === 'p') {
+    if (board[rank][file] === 'empty') {
+      if (
+        file === fromFile &&
+        (rank - fromRank === 1 || (fromRank === 1 && rank - fromRank === 2))
+      ) {
+        legalMoves = true;
+      }
+    } else if (Math.abs(file - fromFile) === 1 && rank - fromRank === 1) {
+      legalMoves = true;
+    }
+  }
+
   return legalMoves;
 }

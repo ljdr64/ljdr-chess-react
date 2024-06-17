bd8a54826b4a25f0f6ae8df6c4fefd907e96ca4a
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-06-17 11:02:59 -0300

    feat: validation for queen movements

--- src/ChessMoves/queenMoves.js
+++ src/ChessMoves/queenMoves.js
@@ -1,18 +1,15 @@
+import { getRookMoves } from './rookMoves';
+import { getBishopMoves } from './bishopMoves';
+
 export function getQueenMoves(fromPiece, square, fromPosition, board) {
   let legalMoves = false;
 
-  const file = square[0].charCodeAt(0) - 'a'.charCodeAt(0);
-  const rank = 8 - square[1];
-
-  const fromFile = fromPosition[0].charCodeAt(0) - 'a'.charCodeAt(0);
-  const fromRank = 8 - fromPosition[1];
-
   if (
-    Math.abs(rank - fromRank) === Math.abs(file - fromFile) ||
-    rank === fromRank ||
-    file === fromFile
+    getRookMoves(fromPiece, square, fromPosition, board) ||
+    getBishopMoves(fromPiece, square, fromPosition, board)
   ) {
     legalMoves = true;
   }
+
   return legalMoves;
 }

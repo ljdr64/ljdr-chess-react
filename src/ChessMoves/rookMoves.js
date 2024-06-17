2ebfe62a0015983a5ecf18cff147bd5db2ed7bb9
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-06-17 10:32:51 -0300

    feat: validation for rook movements

--- src/ChessMoves/rookMoves.js
+++ src/ChessMoves/rookMoves.js
@@ -1,3 +1,41 @@
+/**
+ * Checks if all squares in a row are empty.
+ * @param {array} board - The board represented as a 2D array of characters.
+ * @param {number} rank - The row to check.
+ * @param {number} fromFile - The starting column.
+ * @param {number} file - The ending column.
+ * @returns {boolean} - True if all squares are empty, false otherwise.
+ */
+function areAllSquaresEmptyInRank(board, rank, fromFile, file) {
+  const start = Math.min(fromFile, file);
+  const end = Math.max(fromFile, file);
+  for (let currentFile = start + 1; currentFile < end; currentFile++) {
+    if (board[rank][currentFile] !== 'empty') {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * Checks if all squares in a column are empty.
+ * @param {array} board - The board represented as a 2D array of characters.
+ * @param {number} file - The column to check.
+ * @param {number} fromRank - The starting row.
+ * @param {number} rank - The ending row.
+ * @returns {boolean} - True if all squares are empty, false otherwise.
+ */
+function areAllSquaresEmptyInFile(board, file, fromRank, rank) {
+  const start = Math.min(fromRank, rank);
+  const end = Math.max(fromRank, rank);
+  for (let currentRank = start + 1; currentRank < end; currentRank++) {
+    if (board[currentRank][file] !== 'empty') {
+      return false;
+    }
+  }
+  return true;
+}
+
 export function getRookMoves(fromPiece, square, fromPosition, board) {
   let legalMoves = false;
 
@@ -8,7 +46,12 @@ export function getRookMoves(fromPiece, square, fromPosition, board) {
   const fromRank = 8 - fromPosition[1];
 
   if (rank === fromRank || file === fromFile) {
-    legalMoves = true;
+    if (rank === fromRank) {
+      legalMoves = areAllSquaresEmptyInRank(board, rank, fromFile, file);
+    }
+    if (file === fromFile) {
+      legalMoves = areAllSquaresEmptyInFile(board, file, fromRank, rank);
+    }
   }
   return legalMoves;
 }
